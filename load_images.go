1f101dcb8386712204c790a4e3ea3c2d81848f48
Leandro Nunes <leandronunes.dev@gmail.com>
2019-12-03 19:04:59 -0300

    exclude .keep file

--- load_images.go
+++ load_images.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 const IMAGES_PATH = "./images"
@@ -11,7 +12,7 @@ func loadImages() ([]string, error) {
 	var paths []string
 
 	err := filepath.Walk(IMAGES_PATH, func(path string, info os.FileInfo, err error) error {
-		if !info.IsDir() && path != IMAGES_PATH {
+		if !info.IsDir() && path != IMAGES_PATH && !strings.HasSuffix(path, "keep") {
 			paths = append(paths, path)
 		}
 
